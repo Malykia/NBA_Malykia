9a73e463c4ab40191d47265f144600b71117d994
Malykia <35709896+Malykia@users.noreply.github.com>
2018-02-23 23:38:02 -0500

    Add files via upload

--- NBA/R/correlation_numeric.R
+++ NBA/R/correlation_numeric.R
@@ -2,13 +2,13 @@
 #'
 #' This function allows you to calculate the correlation matrix for all numeric variables in the data for a year
 #' @param year which year do you want to calculate the correlation matrix.
-#' @keywords correlation matrix
-#' @export matrix the correlation matrix for all numeric variables in the data for a year
-#' @examples correlation_numeric(2008)
+#' @keywords correlation 
+#' @export 
+#' @examples
 #' correlation_numeric_function
 
 correlation_numeric <- function(x){
-  year <- filter(nbadata, Year == x)
+  year <- dplyr::filter(nbadata, Year == x)
   continuous <- sapply(nbadata, is.numeric)
   data <- year[ , continuous, drop = FALSE]
   output <- cor(data)
--- NBA/R/oldest_player.R
+++ NBA/R/oldest_player.R
@@ -2,13 +2,13 @@
 #'
 #' This function allows you to find the player for a certain are the oldest.
 #' @param year which year do you want to know the oldest player in.
-#' @keywords oldest player
-#' @export player the player for the year you are interested in who are the oldest.
-#' @examples oldest_player(2008)
+#' @keywords oldest
+#' @export 
+#' @examples 
 #' oldest_player_function()
 
 oldest_player <- function(x){
-  year <- filter(arrange(select(nbadata, Year, Player, Age), desc(Age)), Year == x)
+  year <- dplyr::filter(arrange(select(nbadata, Year, Player, Age), desc(Age)), Year == x)
   output <- year[1, 2]
   return(output)
 }
